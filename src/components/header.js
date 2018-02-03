e81b599914417c9eaacd947e9251cb024fd7177c
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-02-02 20:45:23 -0800

    Initial commit - Header background w/ logo including media queries done

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return(
+    <header>
+      <img src='/photos/header/logo.png' alt='logo'/>
+    </header>
+  )
+}
+
+export default Header;
